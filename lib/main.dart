e6c1d7a526a0e0a61dcddd019a6dd649b38f3385
Neil Mehta <neil.mehta310501@gmail.com>
2020-10-31 00:39:12 +0530

    added fcm notifications feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,51 @@
 import 'package:connectivity_wrapper/connectivity_wrapper.dart';
 import 'package:daysatbits/splashScreen.dart';
 import 'package:flutter/material.dart';
-import 'package:daysatbits/home.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
 
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  String _message = '';
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  _register() {
+    _firebaseMessaging.getToken().then((token) => print(token));
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getMessage();
+  }
+  
+  void getMessage(){
+    _firebaseMessaging.configure(
+        onMessage: (Map<String, dynamic> message) async {
+      print('on message $message');
+      setState(() => _message = message["notification"]["title"]);
+    }, onResume: (Map<String, dynamic> message) async {
+      print('on resume $message');
+      setState(() => _message = message["notification"]["title"]);
+    }, onLaunch: (Map<String, dynamic> message) async {
+      print('on launch $message');
+      setState(() => _message = message["notification"]["title"]);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ConnectivityAppWrapper(
@@ -19,4 +56,4 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
-}
+  }
